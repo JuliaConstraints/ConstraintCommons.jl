be168fa7983c29af11d7bfa57585e2611ab85285
azzaare <jf@baffier.fr>
2023-02-06 11:30:08 +0900

    Add test items and docstrings for symbols concatenation

--- src/symbols.jl
+++ src/symbols.jl
@@ -1,3 +1,12 @@
+"""
+    Base.:*(s1::Symbol, s2::Symbol, connector::AbstractString="_")
+
+Extends `*` to `Symbol`s multiplication by connecting the symbols by an `_`.
+"""
 function Base.:*(s1::Symbol, s2::Symbol, connector::AbstractString="_")
     return Symbol(string(s1) * connector * string(s2))
+end
+
+@testitem "Symbols" tags = [:symbols] begin
+    @test :a * :b === :a_b
 end
