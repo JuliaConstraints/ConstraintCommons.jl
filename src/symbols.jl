bf3ccf462373288dbef70124e9ab25d3993d9238
Azzaare <jf@baffier.fr>
2024-07-19 09:31:02 +0900

    Fix doc

--- src/symbols.jl
+++ src/symbols.jl
@@ -1,5 +1,5 @@
 """
-    Base.:*(s1::Symbol, s2::Symbol, connector::AbstractString="_")
+    symcon(s1::Symbol, s2::Symbol, connector::AbstractString="_")
 
 Extends `*` to `Symbol`s multiplication by connecting the symbols by an `_`.
 """
